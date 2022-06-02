43514c60658d13156eb15a377d28c58fd5b7c54b
Ishraq <ishraqfatin7@gmail.com>
2022-06-02 20:39:09 +0600

    uploaded client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-black text-white text-3xl text-center">
+      <p>Welcome!</p>
+    </div>
+  );
+}
+
+export default App;
