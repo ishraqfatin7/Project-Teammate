e791e71c2b762c0e53dcbee27553a35b0a93bd1b
Shakil Mahmud Arafat <ahmed.arafat666@gmail.com>
2022-06-10 23:28:12 +0600

    initial test- router showing blank page

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,28 @@
-import "./App.css";
+import React from "react";
+
 import HeroHeader from "./components/Hero/HeroHeader";
 import HeroMain from "./components/Hero/HeroMain";
 import Navbar from "./components/Navbar/Navbar";
 import Sidenav from "./components/Navbar/Sidenav";
 import Footer from "./components/Footer";
-const navs = [
-  {
-    item: "Sign In",
-  },
-  { item: "Sign Up" },
-  { item: "Create a team" },
-  { item: "About Us" },
-];
+import Signin from "./components/User/Signin";
+import Home from "./Home";
+
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="drawer dark:bg-slate-900 min-w-full">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content bg-white">
-        <div>
-          <Navbar navs={navs} />
-        </div>
-        <div>
-          <HeroHeader></HeroHeader>
-          <HeroMain></HeroMain>
-          <Footer />
-        </div>
-      </div>
-      <Sidenav navs={navs} />
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/Home" />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+import HeroHeader from "./components/Hero/HeroHeader";
+import HeroMain from "./components/Hero/HeroMain";
+import Navbar from "./components/Navbar/Navbar";
+import Sidenav from "./components/Navbar/Sidenav";
+import Footer from "./components/Footer";
+
+
+let navs = [
+    {
+      item: "Sign In",
+    },
+    { item: "Sign Up" },
+    { item: "Create a team" },
+    { item: "About Us" },
+  ];
+
+export default function Home() {
+  return (
+    <div className="drawer dark:bg-slate-900 min-w-full">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content bg-white">
+        <div>
+          <Navbar navs={navs} />
+        </div>
+        <div>
+          <HeroHeader></HeroHeader>
+          <HeroMain></HeroMain>
+          <Footer />
+        </div>
+      </div>
+      <Sidenav navs={navs} />
+    </div>
+  );
+}
