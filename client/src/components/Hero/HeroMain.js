2198acfebe44c2ac6fad5575048e2d898bec3ff5
Shakil Mahmud Arafat <ahmed.arafat666@gmail.com>
2022-06-23 07:44:07 +0600

    last hope

--- client/src/components/Hero/HeroMain.js
+++ client/src/components/Hero/HeroMain.js
@@ -1,14 +1,10 @@
 import React from "react";
 import HeroCard from "./HeroCard";
-import MiniCard from "./MiniCard";
 import grid1 from "../../images/grid-1.png";
 import grid2 from "../../images/grid2.png";
 import grid3 from "../../images/grid3.png";
 import grid4 from "../../images/grid-4.png";
-import mini1 from "../../images/icons/1.png";
-import mini2 from "../../images/icons/2.png";
-import mini3 from "../../images/icons/3.png";
-import mini4 from "../../images/icons/4.png";
+
 
 let head1 = "Find skilled members or join a team";
 let head2 = "Build Your Team Based on Your choice";
@@ -22,15 +18,7 @@ let text3 =
 let text4 =
   "Build your team and work from home or wherever you want and stay up-to-date";
 
-let cards = [
-  {
-    icon: mini1,
-    text: "Select Catagory",
-  },
-  { icon: mini2, text: "Search Teams" },
-  { icon: mini3, text: "Find or Join" },
-  { icon: mini4, text: "Chat with them" },
-];
+
 const HeroMain = () => {
   return (
     <div>
@@ -41,14 +29,6 @@ const HeroMain = () => {
       <div className="m-auto h-fit lg:flex lg:max-w-7xl lg:items-start">
         <HeroCard grid={grid3} head={head3} text={text3} />
         <HeroCard grid={grid4} head={head4} text={text4} />
-      </div>
-      <div className="mt-5 mb-5" id="how">
-        <h1 className="text-center text-black text-2xl">WONDERING HOW?</h1>
-        <div className="flex justify-center lg:h-45 lg:max-w-2xl m-auto ">
-          {cards.map((card, index) => (
-            <MiniCard image={card.icon} title={card.text} key={index}/>
-          ))}
-        </div>
       </div>
     </div>
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,11 @@ import React from "react";
 import "../App.css";
 import HeroHeader from "./Hero/HeroHeader";
 import HeroMain from "./Hero/HeroMain";
+import MiniCard from "./Hero/MiniCard";
+import mini1 from "../images/icons/1.png";
+import mini2 from "../images/icons/2.png";
+import mini3 from "../images/icons/3.png";
+import mini4 from "../images/icons/4.png";
 import { useAuth } from "../context/authContext";
 import Shell from "./Shell";
 
@@ -14,10 +19,27 @@ export default function Home() {
     { item: "About Us" },
     { item: `${user.user ? "Log out" : ""}` },
   ];
+  let cards = [
+    {
+      icon: mini1,
+      text: "Select Catagory",
+    },
+    { icon: mini2, text: "Search Teams" },
+    { icon: mini3, text: "Find or Join" },
+    { icon: mini4, text: "Chat with them" },
+  ];
   return (
     <Shell navs={navs}>
       <HeroHeader></HeroHeader>
       <HeroMain></HeroMain>
+      <div className="mt-5 mb-5" id="how">
+        <h1 className="text-center text-black text-2xl">WONDERING HOW?</h1>
+        <div className="flex justify-center lg:h-45 lg:max-w-2xl m-auto ">
+          {cards.map((card, index) => (
+            <MiniCard image={card.icon} title={card.text} key={index} />
+          ))}
+        </div>
+      </div>
     </Shell>
   );
 }
